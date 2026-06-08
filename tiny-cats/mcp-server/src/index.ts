251bea9eef38bd61e28f81980de40750d0367da4
Nikhil gupta <ng364357@gmail.com>
2026-06-08 10:43:27 +0530

    feat: update package structure and add recommendCats tool
    - Changed main entry point from index.js to src/index.js in package.json
    - Added development script for nodemon with tsx
    - Introduced axios as a dependency for making HTTP requests
    - Added nodemon and tsx as devDependencies
    - Implemented recommendCats tool in src/index.ts to recommend cat breeds based on user input
    - Created new tool file src/tools/recommendCats.tool.ts for handling cat recommendations

--- tiny-cats/mcp-server/src/index.ts
+++ tiny-cats/mcp-server/src/index.ts
@@ -1,6 +1,7 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+import { recommendCatsTool } from "./tools/recommendCats.tool.ts";
 
 const NWS_API_BASE = "https://api.weather.gov";
 const USER_AGENT = "weather-app/1.0";
@@ -10,3 +11,32 @@ const server = new McpServer({
   name: "tiny-cats",
   version: "1.0.0",
 });
+
+server.registerTool("recommend_cats",
+  {
+    title: "recommend_cats",
+    description: "Recommend a best Cat breed according to inputs",
+    inputSchema: {
+      kidsFriendly: z.boolean(),
+      apartmentFriendly: z.boolean(),
+    },
+  },
+  async ({ kidsFriendly, apartmentFriendly }) => {
+    const result = await recommendCatsTool(kidsFriendly, apartmentFriendly)
+
+    return {
+      content: [
+        {
+          type: "text",
+          text: JSON.stringify(result)
+        }
+      ]
+    }
+  }
+);
+
+const transporter = new StdioServerTransport();
+
+await server.connect(transporter)
+
+console.error("tiny cats mcp running..");
