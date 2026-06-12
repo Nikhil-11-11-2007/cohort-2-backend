4b0d0313521c6efeabf75e1e4cf04cd2d295f85a
Nikhil gupta <ng364357@gmail.com>
2026-06-12 11:12:17 +0530

    feat: update loader animation and enhance overlay text in index.html, main.js, and style.css

--- animations/src/main.js
+++ animations/src/main.js
@@ -1,71 +1,41 @@
 import './style.css'
 import gsap from 'gsap'
 
-// staggerg and gsap timeline
+let count = 0;
 
-// stagger
+const loaderCount = document.querySelector(".loader-count h1")
 
-// gsap.to(".box", {
-//   x: 500,
-//   duration: 1,
-//   ease: "power4.out",
-//   delay: 0.7,
-//   stagger: {
-//     each: 0.1,
-//     from: "random"
-//   }
-// })
+const interval = setInterval(() => {
+  count++
+  loaderCount.innerHTML = `${count}%`
+  if(count === 100) {
+    clearInterval(interval)
+    landingAnimation()
+  }
+}, 20);
 
-// gsap.from("h1 span", {
-//   yPercent: 100,
-//   opacity: 0,
-//   duration: 0.6,
-//   ease: "expo.out",
-//   stagger: {
-//     each: "0.08",
-//     from: "center"
-//   }
-// })
+function landingAnimation() {
+  const tl = gsap.timeline()
 
-// gsap timeline
-
-const tl = gsap.timeline()
-
-// tl.to(".box", {
-//   x: 500,
-//   duration: 1,
-//   ease: "power4.out",
-//   delay: 0.7,
-// }).to(".box1", {
-//   x: 500,
-//   duration: 1,
-//   ease: "power4.out",
-// }, "<").to(".box2", {
-//   x: 500,
-//   duration: 1,
-//   ease: "power4.out",
-// }, "<1").to(".box3", {
-//   x: 500,
-//   duration: 1,
-//   ease: "power4.out",
-// }, "-=0.8")
-
-
-tl.to(".box", {
-  x: 500,
-  duration: 1,
-  ease: "power4.out",
-  delay: 0.7,
-}).to(".box1", {
-  x: 500,
-  duration: 1,
-  ease: "power4.out",
-}, "nik").to(".box2", {
-  x: 500,
-  duration: 1,
-  ease: "power4.out",
-}, "nik-=0.1").to(".box3", {
-  x: 500,
-  duration: 1,
-  ease: "power4.out",
-})
+  tl.to(".loader-count", {
+    opacity: 0,
+    duration: 1,
+    ease: "power3.Out"
+  }).to(".loader", {
+    yPercent: -100,
+    duration: 1.1,
+    ease: "expo.out"
+  }, "-=0.4").from(".background img", {
+    scale: 1.2,
+    duration: 1,
+    ease: "expo.out"
+  }, "-=1").from(".heading h1", {
+    yPercent: 100,
+    duration: 1,
+    ease: "expo.out",
+  },"-=0.8").from(".subheading h2", {
+    yPercent: 100,
+    duration: 1,
+    ease: "expo.out",
+  },"-=0.7")
+}
