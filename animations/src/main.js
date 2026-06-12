020f2bd727f431fc25b0befb53db5343dec051fc
Nikhil gupta <ng364357@gmail.com>
2026-06-12 18:06:17 +0530

    feat: restructure HTML layout and enhance CSS styles for animation pages

--- animations/src/main.js
+++ animations/src/main.js
@@ -1,41 +1,104 @@
 import './style.css'
 import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-let count = 0;
+gsap.registerPlugin(ScrollTrigger);
 
-const loaderCount = document.querySelector(".loader-count h1")
+/**
+ * time control
+ */
 
-const interval = setInterval(() => {
-  count++
-  loaderCount.innerHTML = `${count}%`
-  if(count === 100) {
-    clearInterval(interval)
-    landingAnimation()
+// const play = document.querySelector(".play")
+// const pause = document.querySelector(".pause")
+// const restart = document.querySelector(".restart")
+// const reverse = document.querySelector(".reverse")
+// const seek = document.querySelector(".seek")
+
+// const tl = gsap.timeline({ paused: true })
+
+// tl.to(".box", {
+//   x: 500,
+//   duration: 1,
+//   ease: "power4.out",
+//   delay: 0.7,
+// })
+//   .to(".box1", {
+//     x: 500,
+//     duration: 1,
+//     ease: "power4.out",
+//   }, "nik").addLabel("box1")
+//   .to(".box2", {
+//     x: 500,
+//     duration: 1,
+//     ease: "power4.out",
+//   }, "nik-=0.1")
+//   .to(".box3", {
+//     x: 500,
+//     duration: 1,
+//     ease: "power4.out",
+//   })
+
+
+// play.addEventListener("click", () => {
+//   tl.play()
+// })
+
+// pause.addEventListener("click", () => {
+//   tl.pause()
+// })
+
+// restart.addEventListener("click", () => {
+//   tl.restart()
+// })
+
+// reverse.addEventListener("click", () => {
+//   tl.reverse()
+// })
+
+// seek.addEventListener("click", () => {
+//   tl.seek("box1")
+// })
+
+// scrolltrigger
+
+// gsap.to(".box", {
+//   x:600,
+//   ease: "power4.out",
+//   scrollTrigger: {
+//     trigger: ".page2",
+//     start: "top top",
+//     end: "top -100%",
+//     scrub: true,
+//     pin: true,
+//     onEnter: () => {},
+//     onLeave: () => {},
+//     onUpdate: () => {}
+//   }
+// })
+
+gsap.set(".imageDiv", {
+  scale: 0.3
+})
+
+gsap.set(".content", {
+  gap: "37rem",
+  opacity:0
+})
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page2",
+    start: "top top",
+    end: "top -100%",
+    scrub: true,
+    pin: true,
   }
-}, 20);
+})
 
-function landingAnimation() {
-  const tl = gsap.timeline()
-
-  tl.to(".loader-count", {
-    opacity: 0,
-    duration: 1,
-    ease: "power3.Out"
-  }).to(".loader", {
-    yPercent: -100,
-    duration: 1.1,
-    ease: "expo.out"
-  }, "-=0.4").from(".background img", {
-    scale: 1.2,
-    duration: 1,
-    ease: "expo.out"
-  }, "-=1").from(".heading h1", {
-    yPercent: 100,
-    duration: 1,
-    ease: "expo.out",
-  },"-=0.8").from(".subheading h2", {
-    yPercent: 100,
-    duration: 1,
-    ease: "expo.out",
-  },"-=0.7")
-}
+tl.to(".imageDiv", {
+  scale: 1,
+  ease: "power4.out",
+}).to(".content", {
+  gap: "7rem",
+  opacity:1
+}, "<")
