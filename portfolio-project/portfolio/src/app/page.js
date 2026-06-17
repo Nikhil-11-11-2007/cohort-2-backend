543e276a33e0652bb89992852e1850da34874493
Nikhil gupta <ng364357@gmail.com>
2026-06-17 12:48:48 +0530

    feat: implement TextReveal component with GSAP animations and hover effects

--- portfolio-project/portfolio/src/app/page.js
+++ portfolio-project/portfolio/src/app/page.js
@@ -1,8 +1,28 @@
+"use client"
+
+import TextReveal from "@/components/TextReveal";
+import { useRef } from "react";
 
 export default function Home() {
+
+  const triggerRef = useRef(null)
+
+  const handleHover = () => {
+
+    triggerRef.current?.play()
+
+  }
+
+  const handleLeave = () => {
+    triggerRef.current?.reverse()
+  }
+
   return (
     <main className="bg-blue-950 h-[300vh]">
-      <h1 className="text-9xl">hello world</h1>
+      <div onPointerEnter={handleHover} onPointerLeave={handleLeave} className="h-[15rem] w-[16rem] bg-red-400"></div>
+      <TextReveal ref={triggerRef} splitBy="chars" trigger="manual" scrollStart="top 20%" className="text-9xl">
+        Hello World
+      </TextReveal>
     </main>
   );
 }
