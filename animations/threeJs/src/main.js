d99c856b61b9d3835d034341d3a163c61e7ddf19
Nikhil gupta <ng364357@gmail.com>
2026-06-22 12:05:10 +0530

    feat: enhance texture loading and update cube geometry in Three.js setup

--- animations/threeJs/src/main.js
+++ animations/threeJs/src/main.js
@@ -13,6 +13,28 @@ const size = {
 const scene = new THREE.Scene()
 const clock = new THREE.Clock()
 
+// texture Loader
+
+const textureLoader = new THREE.TextureLoader()
+
+const texture = textureLoader.load(
+  "https://images.unsplash.com/photo-1781112981218-db02b34ce0ac?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  () => {
+    console.log("Texture is loaded")
+  },
+  () => {
+    console.log("Texture is loading...")
+  },
+  () => {
+    console.log("error in texture loading")
+  }
+)
+
+const texture2 = textureLoader.load(
+  "https://images.unsplash.com/photo-1781460661007-48781a25a962?q=80&w=685&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+
+)
+
 // CAMERA
 
 const camera = new THREE.PerspectiveCamera(
@@ -26,15 +48,28 @@ camera.position.z = 5
 
 // MESH (3D object)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+const geometry = new THREE.BoxGeometry(2, 2, 2)
+// const geometry = new THREE.CylinderGeometry( 1, 1, 4, 32 );
 const material = new THREE.MeshBasicMaterial({
-  color: "rgba(225, 10, 7, 0.72)"
+  // color: "rgba(225, 10, 7, 0.72)"
+  map: texture2
 })
 
 const cube = new THREE.Mesh(geometry, material)
 
-cube.rotation.y = 1.2
-cube.rotation.x = 1.2
+// position of the cube
+
+// cube.position.x = 1.5
+// cube.position.set(1.5,-1,1.3)
+
+// scale of the cube 
+
+// cube.scale.x = 1.5
+// cube.scale.set(1.5,-2,1.3)
+
+// rotation of the cube
+
+// cube.rotation.x = Math.PI / 3
 
 scene.add(cube)
 
@@ -48,7 +83,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas,
 })
 
-const controls = new OrbitControls( camera, renderer.domElement ); 
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true
 
 renderer.setSize(size.width, size.height);
@@ -63,17 +98,17 @@ window.addEventListener("resize", () => {
   renderer.setSize(size.width, size.height)
 })
 
-  // renderer.render(scene,camera)
+// renderer.render(scene,camera)
 
 const animate = () => {
-  
+
   const delta = clock.getElapsedTime()
 
-  cube.rotation.y = delta;
+  // cube.rotation.y = delta;
 
   controls.update()
 
-  renderer.render(scene,camera)
+  renderer.render(scene, camera)
 
   requestAnimationFrame(animate);
 
