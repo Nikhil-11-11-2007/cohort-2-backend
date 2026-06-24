753d8be377e399a4587bfafa4894083540d14e6f
Nikhil gupta <ng364357@gmail.com>
2026-06-24 17:12:35 +0530

    Refactor code structure for improved readability and maintainability

--- animations/react-3-fiber/src/components/Experience.jsx
+++ animations/react-3-fiber/src/components/Experience.jsx
@@ -1,19 +1,18 @@
-import { useTexture } from '@react-three/drei'
+import { Environment, useGLTF, useTexture } from '@react-three/drei'
 import { useFrame, useLoader } from '@react-three/fiber'
 import React, { useRef } from 'react'
 import * as THREE from "three"
-import { texture } from 'three/src/nodes/accessors/TextureNode.js'
 
 const Experience = () => {
 
     const cubeRef = useRef(null)
 
     useFrame((state, delta) => {
-        cubeRef.current.rotation.y += delta
+        // cubeRef.current.rotation.y += delta
     })
 
     // use this TextureLoader
-    const {texture,texture2} = useTexture(
+    const { texture, texture2 } = useTexture(
         {
             texture: "https://images.unsplash.com/photo-1779896411942-ea4ca54de043?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             texture2: "https://images.unsplash.com/photo-1781461565715-887bd369f481?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -25,13 +24,25 @@ const Experience = () => {
     //     "https://images.unsplash.com/photo-1779896411942-ea4ca54de043?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     // )
 
+    const { scene } = useGLTF("./model.glb")
+
+    const handelClick = () => {
+        cubeRef.current.material.color.set("green")
+    }
+
     return (
         <>
-            <mesh ref={cubeRef}>
+            <mesh onClick={handelClick} ref={cubeRef}>
                 <boxGeometry args={[2, 2, 2]} />
-                {/* <torusGeometry args={[1.5, 0.5, 16, 100]} /> */}
-                <meshBasicMaterial map={texture2} color={""} />
+                {/* <meshBasicMaterial map={texture2} color={""} /> */}
+                <meshStandardMaterial roughness={0.01} metalness={0.9} color={"red"} />
             </mesh>
+
+            {/* <ambientLight intensity={3} color={"#fffff"} />
+
+            <primitive object={scene} position={[0,-2,0]} /> */}
+
+            <Environment files='./envMap.hdr' />
 
         </>
     )
