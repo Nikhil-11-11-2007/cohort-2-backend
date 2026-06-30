7550468f8723fc1ec3eab737c6fa120e42ee0bab
Nikhil gupta <ng364357@gmail.com>
2026-06-30 18:31:53 +0530

    feat: add PWA support with Vite PWA plugin and register service worker
    - Updated package.json to include vite-plugin-pwa dependency.
    - Modified main.jsx to register the service worker immediately.
    - Enhanced vite.config.js to configure PWA settings including manifest and icons.
    - Added image1.png and image2.png for PWA icons in the public directory.

--- animations/rf3-project/vite.config.js
+++ animations/rf3-project/vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA({
+    registerType: "autoUpdate",
+    manifest: {
+      name: "offline-fan gallery",
+      short_name: "fan-gallery",
+      description: "this is 3D fan gallery",
+      theme_color: "#111",
+      background_color: "#111",
+      icons: [
+        {
+          src: "image1.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "image2.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
+    },
+  }),
+  ],
+});
