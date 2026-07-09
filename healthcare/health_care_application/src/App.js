995fa415143dae55acfd7bacc6e5b21598042f74
Nikhil gupta <ng364357@gmail.com>
2026-07-09 16:30:44 +0530

    feat: integrate wallet connection in Healthcare component and add contract ABI

--- healthcare/health_care_application/src/App.js
+++ healthcare/health_care_application/src/App.js
@@ -1,19 +1,7 @@
-import { useEffect } from 'react';
 import './App.css';
 import Healthcare from './HealthCare';
 
 function App() {
-
-  useEffect(() => {
-    const connectWallet = async () => {
-      if(!window.ethereum) {
-        alert("download metaMask wallet")
-        return
-      }
-    }
-
-    connectWallet()
-  },[])
 
   return (
     <div className="App">
--- healthcare/health_care_application/src/HealthCare.js
+++ healthcare/health_care_application/src/HealthCare.js
@@ -1,4 +1,232 @@
+import { useEffect, useState } from "react";
+import { ethers, BrowserProvider } from 'ethers'
+
 const Healthcare = () => {
+
+    const [accountAddress, setAccountAddress] = useState("");
+    const contractAddress = "0xA6063Df651f138b033049E41bf24433Cf2BA788A";
+    const contractAbi = [
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "patient_id",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "patient_name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "diagnosis",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "treatment",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "age",
+                    "type": "uint256"
+                }
+            ],
+            "name": "addPatientRecord",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "user",
+                    "type": "address"
+                }
+            ],
+            "name": "authorizeByOwner",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "name": "allRecords",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "record_id",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "age",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "string",
+                    "name": "patient_name",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "diagnosis",
+                    "type": "string"
+                },
+                {
+                    "internalType": "string",
+                    "name": "treatment",
+                    "type": "string"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "timestamp",
+                    "type": "uint256"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "authorizePerson",
+            "outputs": [
+                {
+                    "internalType": "bool",
+                    "name": "",
+                    "type": "bool"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "patient_id",
+                    "type": "uint256"
+                }
+            ],
+            "name": "featchPatientRecords",
+            "outputs": [
+                {
+                    "components": [
+                        {
+                            "internalType": "uint256",
+                            "name": "record_id",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "age",
+                            "type": "uint256"
+                        },
+                        {
+                            "internalType": "string",
+                            "name": "patient_name",
+                            "type": "string"
+                        },
+                        {
+                            "internalType": "string",
+                            "name": "diagnosis",
+                            "type": "string"
+                        },
+                        {
+                            "internalType": "string",
+                            "name": "treatment",
+                            "type": "string"
+                        },
+                        {
+                            "internalType": "uint256",
+                            "name": "timestamp",
+                            "type": "uint256"
+                        }
+                    ],
+                    "internalType": "struct Record[]",
+                    "name": "",
+                    "type": "tuple[]"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "getOwner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "name": "owner",
+            "outputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ];
+
+    useEffect(() => {
+        const connectWallet = async () => {
+            if (!window.ethereum) {
+                alert("download metaMask wallet")
+                return
+            }
+
+            const provider = new ethers.BrowserProvider(window.ethereum)
+            await window.ethereum.request({
+                method: "wallet_switchEthereumChain",
+                params: [{ chainId: "0xaa36a7" }],
+            });
+
+            const signer = await provider.getSigner();
+            setAccountAddress(await signer.getAddress())
+
+            const smartContract = new ethers.Contract(contractAddress,contractAbi,accountAddress)
+
+        }
+
+        connectWallet()
+    }, [])
+
     return (
         <div className='container'>
             <h1 className="title">HealthCare Application</h1>
