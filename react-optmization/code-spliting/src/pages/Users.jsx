f4a50645807aeff9637e86babebceac1553a7b5a
Nikhil gupta <ng364357@gmail.com>
2026-07-25 17:52:58 +0530

    feat: display user list by mapping over user data in Users component

--- react-optmization/code-spliting/src/pages/Users.jsx
+++ react-optmization/code-spliting/src/pages/Users.jsx
@@ -2,10 +2,28 @@ import React from 'react'
 import UserCard from '../components/UserCard'
 
 const Users = () => {
+
+  const users = [
+    {id:1, name: "test1"},
+    {id:2, name: "test2"},
+    {id:3, name: "test3"}
+  ]
+
+  console.log(users)
+
   return (
     <div>
 
-      <UserCard />
+      {/* <UserCard /> */}
+      {
+        users.map((user,idx) => {
+          return <div key={user.id} className='w-full p-2'>
+            <div className='h-[30vh] w-[20vw] border-1 '>
+              <h1>{user.name}</h1>
+            </div>
+          </div>
+        })
+      }
 
     </div>
   )
