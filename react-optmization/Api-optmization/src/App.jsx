910e75e593cb26c55d90635fe846c4dc1e449ab2
Nikhil gupta <ng364357@gmail.com>
2026-07-31 13:31:28 +0530

    feat: initialize api-optimization project with React, Vite, and Tailwind CSS

---
+++ react-optmization/Api-optmization/src/App.jsx
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import PaginationButton from './components/PaginationButton'
+
+const App = () => {
+
+  const [postData, setPostData] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postPerPage, setPostPerPage] = useState(10)
+
+  const fetchData = async () => {
+
+
+    const res = await axios.get("https://dummyjson.com/products")
+    setPostData(res.data.products)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  // console.log(postData)
+
+  const lastPostIndex = currentPage * postPerPage
+  const firstPostIndex = lastPostIndex - postPerPage
+  const currentPost = postData.slice(firstPostIndex, lastPostIndex)
+
+  return (
+    <div className='min-h-screen bg-black text-white w-full flex flex-col'>
+      <div className='min-h-screen w-full flex flex-wrap px-21 items-center gap-5 bg-black text-white'>
+
+        {currentPost.map((item) => {
+          return <div key={item.id} className='w-80 h-75 bg-red-900 rounded-md flex items-center justify-center flex-col '>
+            <img className='w-30' src={item.images} alt="" />
+            <h1 className='text-center'>{item.title}</h1>
+          </div>
+        })}
+
+
+        <PaginationButton />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ react-optmization/Api-optmization/src/components/PaginationButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PaginationButton = () => {
+  return (
+    <div className='w-full my-14'>
+        <button className='border-1 px-5 py-3 rounded-md'>1</button>
+    </div>
+  )
+}
+
+export default PaginationButton
