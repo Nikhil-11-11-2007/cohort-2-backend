ab536ba639c0d9a24ff49e3e7d69edce0a201722
Nikhil gupta <ng364357@gmail.com>
2026-08-04 12:39:23 +0530

    feat: add fetchUsers API function and integrate with App component

--- react-optmization/tanstack-query/src/App.jsx
+++ react-optmization/tanstack-query/src/App.jsx
@@ -1,8 +1,27 @@
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
+import { fetchUsers } from './api/api'
 
 const App = () => {
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchUsers
+  })
+
+  
+  if (isLoading) return <p>Loading...</p>
+  if (isError) return <p>Error...</p>
+  console.log(data)
+  
   return (
-    <div>App</div>
+    <div>
+      {data.map((user) => (
+        <div key={user.id}>
+          <h1>{user.name}</h1>
+        </div>
+      ))}
+    </div>
   )
 }
 
